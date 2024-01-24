66123f82285d406cd62922f77ac54276601dad06
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2024-01-24 21:54:02 +0700

    AddDatabaseDeveloperPageExceptionFilter only if environment is development.

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -8,7 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+// Disable on non development.
+if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development)
+{
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+}
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
